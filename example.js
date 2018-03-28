d7d593fabb3da6459a427a14c3f4abda2eed62ee
Tanner Hoisington <Tanner@Tanner-MBP.local>
2018-03-28 18:03:06 +0200

    Adopted new endpoint function model, added some helper functions

--- example.js
+++ example.js
@@ -3,19 +3,39 @@
 require("dotenv").config();
 
 const Clockodo = require("./lib/api");
+const prompt = require("prompt"); // eslint-disable-line import/no-extraneous-dependencies
 
 const clockodo = new Clockodo(process.env.CLOCKODO_USER, process.env.CLOCKODO_API_KEY);
-const params = {
-    time_since: "2017-01-01 00:00:00",
-    time_until: "2017-02-01 00:00:00",
-    "filter[billable]": 0,
-};
 
-clockodo
-    .getEntries(params)
-    .then(data => {
-        console.log(data);
-    })
-    .catch(error => {
-        console.log("caught", error);
-    });
+prompt.start();
+
+prompt.get("testFunction", (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+
+    if (result.testFunction === "getEntries") {
+        const parameters = {
+            billable: 2,
+            userId: 38557,
+        };
+
+        clockodo
+            .getEntries("03-12-2016", "08-18-2017", parameters)
+            .then(data => {
+                console.log(data);
+            })
+            .catch(error => {
+                console.log("caught", error);
+            });
+    } else if (result.testFunction === "getUsers") {
+        clockodo
+            .getUsers()
+            .then(data => {
+                console.log(data);
+            })
+            .catch(error => {
+                console.log("caught", error);
+            });
+    }
+});
