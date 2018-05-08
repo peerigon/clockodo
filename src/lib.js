6e59ee44db90b27b7e7dbddafe53c35caeb1fbf3
Tanner Hoisington <hoisington.tanner@gmail.com>
2018-05-08 18:52:20 +0200

    fix: fixes axios put sending the wrong data format for Clockodo

--- src/lib.js
+++ src/lib.js
@@ -37,7 +37,9 @@ class ClockodoLib {
     }
     async put(resource, params = {}) {
         const mappedObj = mapKeys(params);
-        const response = await this[axiosClient].put(resource, mappedObj);
+        const response = await this[axiosClient].put(resource, qs.stringify(mappedObj), {
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        });
 
         return deepMapKeys(response.data, key => camelCase(key));
     }
