b177c1a20086227c83441bc317a3a9bd1cb3c307
Tanner Hoisington <hoisington.tanner@gmail.com>
2018-10-10 14:43:05 +0200

    chore: sets automatic generation of types files to false as I see no benefit

--- dist/lib.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class ClockodoLib {
-    constructor(user: any, apiKey: any);
-    get(resource: any, params?: {}): Promise<{}>;
-    post(resource: any, params?: {}): Promise<{}>;
-    put(resource: any, params?: {}): Promise<{}>;
-    delete(resource: any, params?: {}): Promise<{}>;
-}
