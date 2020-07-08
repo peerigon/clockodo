4e3a1212daefeca47c708e3c223a2d35b7de8d96
aubcel <celestine.auburger@peerigon.com>
2020-07-08 13:30:20 +0200

    fix cache

--- src/plugins/cache.ts
+++ src/plugins/cache.ts
@@ -1,15 +1,15 @@
 import {setup} from "axios-cache-adapter";
 import {Clockodo} from "../internals/api";
-import {axiosClient} from "../internals/utilities/symbols";
+import {axiosClient, clockodoApi} from "../internals/utilities/symbols";
 
 const cachePlugin = (config: {cacheTime: number}) => (clockodo: Clockodo) => {
     if (typeof config.cacheTime !== "number") {
         throw new Error("Clockodo cacheTime expected to be a number, is typeof: " + typeof config.cacheTime);
     }
-    clockodo[axiosClient] =
+    clockodo[clockodoApi][axiosClient] =
         setup({
-            baseURL: clockodo[axiosClient].defaults.baseURL,
-            headers: clockodo[axiosClient].defaults.headers,
+            baseURL: clockodo[clockodoApi][axiosClient].defaults.baseURL,
+            headers: clockodo[clockodoApi][axiosClient].defaults.headers,
             cache: {
                 maxAge: config.cacheTime,
                 exclude: {query: false},
