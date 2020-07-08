f4bcfb9ad7c632f63ed294ce0b8fac9b6818f0ee
aubcel <celestine.auburger@peerigon.com>
2020-07-08 12:58:53 +0200

    remove dist and husky

--- dist/internals/utilities/symbols.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const clockodoApi: unique symbol;
-export declare const axiosClient: unique symbol;
--- dist/plugins/cache.d.ts
+++
@@ -1,5 +0,0 @@
-import { Clockodo } from "../internals/api";
-declare const cachePlugin: (config: {
-    cacheTime: number;
-}) => (clockodo: Clockodo) => void;
-export default cachePlugin;
