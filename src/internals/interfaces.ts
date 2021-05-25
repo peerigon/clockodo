abde22e266279e02819e5ac6845d422a01cef783
Johannes Ewald <johannes.ewald@peerigon.com>
2021-05-25 14:56:42 +0200

    Add deprecated flag to task interface

--- src/internals/interfaces.ts
+++ src/internals/interfaces.ts
@@ -5,7 +5,6 @@ import {
     TimeEntryBillability,
 } from "./enums";
 
-// TODO: Remove deprecated?
 export type Customer = {
     id: number;
     name: string;
@@ -154,29 +153,53 @@ export type LumpsumServiceEntry = CommonEntry & {
 
 export type Entry = TimeEntry | LumpsumValueEntry | LumpsumServiceEntry;
 
+/** @deprecated */
 export type Task = {
+    /** @deprecated */
     day: string;
+    /** @deprecated */
     customersId: number;
+    /** @deprecated */
     customersName: string;
+    /** @deprecated */
     projectsId: number | 0;
+    /** @deprecated */
     projectsName: string | null;
+    /** @deprecated */
     servicesId: number | null;
+    /** @deprecated */
     servicesName: string | null;
+    /** @deprecated */
     lumpsumsId: number | null;
+    /** @deprecated */
     lumpsumsAmount: number | null;
+    /** @deprecated */
     lumpsumsName: string | null;
+    /** @deprecated */
     lumpsumsPrice: number | null;
+    /** @deprecated */
     lumpsumsUnit: string | null;
+    /** @deprecated */
     billable: 0 | 1;
+    /** @deprecated */
     textsId: number | 0;
+    /** @deprecated */
     text: string | null;
+    /** @deprecated */
     timeSince: string;
+    /** @deprecated */
     timeUntil: string;
+    /** @deprecated */
     duration: number;
+    /** @deprecated */
     durationTime: string;
+    /** @deprecated */
     durationText: string;
+    /** @deprecated */
     isClocking: boolean;
+    /** @deprecated */
     hasJustLumpsums: boolean;
+    /** @deprecated */
     revenue?: number;
 };
 
