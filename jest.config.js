a135b517a8595d7c3d7772a2f2a612516858f910
Johannes Ewald <johannes.ewald@peerigon.com>
2021-10-09 01:09:34 +0200

    Ignore dist folder in Jest tests

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-    preset: "ts-jest",
-    setupFiles: ["dotenv/config"],
-    collectCoverage: true,
-    resolver: "ts-jest-resolver",
-    globals: {
-        "ts-jest": {
-            diagnostics: false,
-        },
+  preset: "ts-jest",
+  setupFiles: ["dotenv/config"],
+  testPathIgnorePatterns: ["<rootDir>/dist/", "<rootDir>/node_modules/"],
+  collectCoverage: true,
+  resolver: "ts-jest-resolver",
+  globals: {
+    "ts-jest": {
+      diagnostics: false,
     },
+  },
 };
