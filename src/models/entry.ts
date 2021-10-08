d566218ee243c6efa0886a9240c78907d4a1019d
Johannes Ewald <johannes.ewald@peerigon.com>
2021-10-08 17:40:23 +0200

    feat: Add entry utils

--- src/models/entry.ts
+++ src/models/entry.ts
@@ -33,9 +33,6 @@ type CommonEntry = {
   timeLastChange: string;
 };
 
-// Initially we've split up TimeEntry here into ClockingTimeEntry | ClockedTimeEntry | ManualTimeEntry
-// We had to revert this because the shared 'type' tag property does not differentiate between
-// these types. Without a shared tag property, it's impossible for TypeScript to infer the correct type.
 export type TimeEntry = CommonEntry & {
   type: EntryType.Time;
   servicesId: number;
@@ -49,6 +46,27 @@ export type TimeEntry = CommonEntry & {
   clockedOffline: boolean;
   /** Only present with sufficient access rights */
   hourlyRate?: number;
+};
+
+export type ClockingTimeEntry = TimeEntry & {
+  timeUntil: null;
+  timeClockedSince: string;
+  clocked: true;
+};
+
+export type ClockedTimeEntry = TimeEntry & {
+  timeUntil: string;
+  timeClockedSince: string;
+  clocked: true;
+};
+
+export type ClockTimeEntry = ClockingTimeEntry | ClockedTimeEntry;
+
+export type ManualTimeEntry = TimeEntry & {
+  timeUntil: string;
+  timeClockedSince: null;
+  clocked: false;
+  clockedOffline: false;
 };
 
 export type LumpsumValueEntry = CommonEntry & {
@@ -67,4 +85,39 @@ export type LumpsumServiceEntry = CommonEntry & {
   lumpsumServicesAmount: number;
 };
 
-export type Entry = TimeEntry | LumpsumValueEntry | LumpsumServiceEntry;
+export type LumpsumEntry = LumpsumValueEntry | LumpsumServiceEntry;
+
+export type Entry = TimeEntry | LumpsumEntry;
+
+export const isClockingTimeEntry = (
+  entry: Entry
+): entry is ClockingTimeEntry => {
+  return entry.type === EntryType.Time && entry.timeUntil === null;
+};
+
+export const isClockedTimeEntry = (entry: Entry): entry is ClockedTimeEntry => {
+  return (
+    entry.type === EntryType.Time &&
+    entry.clocked === true &&
+    typeof entry.timeUntil === "string"
+  );
+};
+
+export const isClockTimeEntry = (entry: Entry): entry is ClockTimeEntry => {
+  return isClockedTimeEntry(entry) || isClockingTimeEntry(entry);
+};
+
+export const isManualTimeEntry = (entry: Entry): entry is ClockTimeEntry => {
+  return entry.type === EntryType.Time && entry.clocked === false;
+};
+
+export const isTimeEntry = (entry: Entry): entry is TimeEntry => {
+  return isClockTimeEntry(entry) || isManualTimeEntry(entry);
+};
+
+export const isLumpsumEntry = (entry: Entry): entry is LumpsumEntry => {
+  return (
+    entry.type === EntryType.LumpsumValue ||
+    entry.type === EntryType.LumpsumService
+  );
+};
