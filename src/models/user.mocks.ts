33d3ed67d068e41bff439e0e8e96e0fc8000e8be
Johannes Ewald <johannes.ewald@peerigon.com>
2022-04-02 19:07:25 +0200

    fix: Replace deprecated faker functions

--- src/models/user.mocks.ts
+++ src/models/user.mocks.ts
@@ -3,14 +3,13 @@ import { User, UserRole, UserLanguage } from "./user.js";
 
 export const createUserMocks = ({ count }: { count: number }) =>
   Array.from({ length: count }, (_, index): User => {
-    const card = faker.helpers.createCard();
     const userId = index;
 
     return {
       id: userId,
-      name: card.name,
+      name: faker.name.findName(),
       number: faker.datatype.hexaDecimal(),
-      email: card.email,
+      email: faker.internet.email(),
       role: [UserRole.Owner, UserRole.Manager, UserRole.Worker][index % 3],
       active: faker.datatype.number({ min: 0, max: 10 }) > 2,
       timeformat12h: faker.datatype.boolean(),
