512d749288ef062c546677dcf317d51b00d12bb7
Johannes Ewald <johannes.ewald@peerigon.com>
2022-06-08 18:40:21 +0200

    Remove deprecated cache feature
    BREAKING CHANGE: The cache plugin has been removed. It has been deprecated for quite some time now and will be discontinued.

--- src/clockodo.test.ts
+++ src/clockodo.test.ts
@@ -9,7 +9,6 @@ import {
   AbsenceStatus,
   mapRequestBody,
 } from "./index.js";
-import { cachePlugin } from "./plugins/cache.js";
 
 const CLOCKODO_API = "https://my.clockodo.com/api";
 const config: Config = {
@@ -93,45 +92,6 @@ describe("Clockodo (instance)", () => {
       ).toThrowErrorMatchingInlineSnapshot(
         `"baseUrl should be undefined or a string but given value 5678 is typeof number"`
       );
-    });
-  });
-
-  describe("Cache", () => {
-    it("should cache the first request for cacheTime", async () => {
-      const clockodoWithCache = new Clockodo(config);
-
-      clockodoWithCache.use(cachePlugin({ cacheTime: 50 }));
-
-      const usersId = 7;
-      let requestCounter = 0;
-
-      const nockScope = nock(CLOCKODO_API)
-        .get(`/users/${usersId}`)
-        .twice()
-        .reply(200, () => {
-          requestCounter++;
-
-          return {};
-        });
-
-      await clockodoWithCache.getUser({
-        id: usersId,
-      });
-      expect(requestCounter).toBe(1);
-      await clockodoWithCache.getUser({
-        id: usersId,
-      });
-      // If cache is not working this would fail
-      expect(requestCounter).toBe(1);
-
-      await new Promise((resolve) => setTimeout(resolve, 50));
-
-      await clockodoWithCache.getUser({
-        id: usersId,
-      });
-      expect(requestCounter).toBe(2);
-
-      nockScope.done();
     });
   });
 
