e986bf0c5c50f661454449c35bc8c79adbb01ff3
Johannes Ewald <johannes.ewald@peerigon.com>
2022-06-22 19:18:55 +0200

    feat: Implement targethours mocks

---
+++ src/lib/mocks.ts
@@ -0,0 +1,117 @@
+import { faker } from "@faker-js/faker";
+
+export const ONE_DAY = 24 * 60 * 60 * 1000;
+const MAX_ITERATION_COUNT = 10000;
+
+export const isoDateFromDateTime = (dateTime: Date) => {
+  return dateTime.toISOString().replace(/T.*/, "");
+};
+
+export const isoMonthFromDateTime = (dateTime: Date) => {
+  return dateTime.toISOString().replace(/-\d\dT.*/, "");
+};
+
+export const isoDateFromTimestamp = (timestamp: number) => {
+  return isoDateFromDateTime(new Date(timestamp));
+};
+
+export const timestampFromIsoDate = (isoDate: string) => {
+  return new Date(isoDate).getTime();
+};
+
+export const endOfMonth = (date: Date) => {
+  let startOfNextMonth = new Date(date.getTime());
+
+  startOfNextMonth.setMonth(date.getMonth() + 1);
+
+  if (startOfNextMonth.getMonth() === 0) {
+    startOfNextMonth.setFullYear(date.getFullYear() + 1);
+  }
+
+  startOfNextMonth = new Date(isoMonthFromDateTime(startOfNextMonth));
+
+  const endOfMonth = new Date(startOfNextMonth);
+
+  endOfMonth.setTime(endOfMonth.getTime() - ONE_DAY / 2);
+
+  return new Date(isoDateFromDateTime(endOfMonth));
+};
+
+export const nextDay = (date: Date) => {
+  return new Date(isoDateFromDateTime(new Date(date.getTime() + ONE_DAY)));
+};
+
+export const generateUnique = <Value>({
+  count,
+  generate,
+}: {
+  count: number;
+  generate: () => Value;
+}) => {
+  const generated = new Set<Value>();
+
+  for (let i = 0; generated.size < count; i++) {
+    if (i === MAX_ITERATION_COUNT) {
+      throw new Error(
+        `Couldn't generate enough unique values before reaching the max iteration count.
+This usually happens when the generated values create too many conflicts (e.g. too many dates on a small date range).`
+      );
+    }
+    generated.add(generate());
+  }
+
+  return generated;
+};
+
+export const generateDayRange = ({
+  count,
+  between: [from, to],
+}: {
+  count: number;
+  between: [Date, Date];
+}) => {
+  return Array.from(
+    generateUnique({
+      count,
+      generate: () => {
+        const randomDay = isoDateFromDateTime(faker.date.between(from, to));
+
+        return timestampFromIsoDate(randomDay);
+      },
+    })
+  ).sort();
+};
+
+export const generateMonthRange = ({
+  count,
+  between: [from, to],
+}: {
+  count: number;
+  between: [Date, Date];
+}) => {
+  return Array.from(
+    generateUnique({
+      count,
+      generate: () => {
+        const randomMonth = isoMonthFromDateTime(faker.date.between(from, to));
+
+        return timestampFromIsoDate(randomMonth);
+      },
+    })
+  ).sort();
+};
+
+export const toPairs = <Item>(array: Array<Item>) => {
+  if (array.length % 2 !== 0) {
+    throw new Error("Cannot create pairs: Array length must be divisible by 2");
+  }
+
+  const count = array.length / 2;
+  const pairs = new Array<[Item, Item]>(count);
+
+  for (let i = 0; i < count; i++) {
+    pairs[i] = [array[i * 2], array[i * 2 + 1]];
+  }
+
+  return pairs;
+};
---
+++ src/models/targethours.mocks.ts
@@ -0,0 +1,165 @@
+import { faker } from "@faker-js/faker";
+import {
+  generateDayRange,
+  generateMonthRange,
+  isoDateFromDateTime,
+  isoDateFromTimestamp,
+  toPairs,
+  endOfMonth,
+  nextDay,
+} from "../lib/mocks.js";
+import {
+  TargethoursRow,
+  TargethoursRowMonthly,
+  TargethoursRowType,
+  TargethoursRowWeekly,
+} from "./targethours.js";
+
+type CommonOptions = {
+  count?: number;
+  dateSinceBetween?: [Date, Date];
+};
+
+const DEFAULT_FROM = new Date("2019");
+const DEFAULT_TO = new Date("2022");
+
+const createCommonTargethoursRowMock = (dateSince: Date) => {
+  return {
+    id: 0,
+    dateSince: isoDateFromDateTime(dateSince),
+    dateUntil: null,
+    compensationMonthly: faker.datatype.number({ min: 0, max: 8 }),
+    usersId: 0,
+  };
+};
+
+export const createTargethoursRowWeeklyMocks = ({
+  count = 1,
+  dateSinceBetween: [from, to] = [DEFAULT_FROM, DEFAULT_TO],
+}: CommonOptions = {}) => {
+  const dayPairs = toPairs(
+    generateDayRange({
+      count: count * 2,
+      between: [from, to],
+    })
+  );
+  let previousTo: undefined | number;
+
+  return dayPairs.map(([from, to], index): TargethoursRowWeekly => {
+    const dateSince =
+      // Make it more unlikely that there are "holes" between to and from
+      previousTo !== undefined && faker.datatype.number({ min: 0, max: 10 }) < 7
+        ? nextDay(new Date(previousTo))
+        : new Date(from);
+    const commonTargethoursRow = createCommonTargethoursRowMock(dateSince);
+    const isLastOne = index === count - 1;
+    const typicalHours = [
+      0,
+      0,
+      faker.datatype.number({ min: 1, max: 4 }),
+      faker.datatype.number({ min: 4, max: 8 }),
+      parseFloat(faker.datatype.float({ min: 0, max: 8 }).toFixed(2)),
+      24,
+    ];
+
+    faker.helpers.shuffle(typicalHours);
+
+    previousTo = to;
+
+    return {
+      ...commonTargethoursRow,
+      id: index,
+      dateUntil:
+        isLastOne && faker.datatype.boolean() ? null : isoDateFromTimestamp(to),
+      type: TargethoursRowType.Weekly,
+      monday: typicalHours[0 % typicalHours.length],
+      tuesday: typicalHours[1 % typicalHours.length],
+      wednesday: typicalHours[2 % typicalHours.length],
+      thursday: typicalHours[3 % typicalHours.length],
+      friday: typicalHours[4 % typicalHours.length],
+      saturday: typicalHours[5 % typicalHours.length],
+      sunday: typicalHours[6 % typicalHours.length],
+      absenceFixedCredit: faker.datatype.boolean(),
+      compensationDaily: faker.datatype.number({ min: 0, max: 60 }),
+    };
+  });
+};
+
+export const createTargethoursRowMonthlyMocks = ({
+  count = 1,
+  dateSinceBetween: [from, to] = [DEFAULT_FROM, DEFAULT_TO],
+}: CommonOptions = {}) => {
+  const monthPairs = toPairs(
+    generateMonthRange({
+      count: count * 2,
+      between: [from, to],
+    })
+  );
+
+  return monthPairs.map(([from, to], index): TargethoursRowMonthly => {
+    const commonTargethoursRow = createCommonTargethoursRowMock(new Date(from));
+    const isLastOne = index === count - 1;
+    const typicalHours = [
+      0,
+      0,
+      faker.datatype.number({ min: 1, max: 4 }),
+      faker.datatype.number({ min: 4, max: 8 }),
+      parseFloat(faker.datatype.float({ min: 0, max: 8 }).toFixed(2)),
+      24,
+    ];
+
+    faker.helpers.shuffle(typicalHours);
+
+    return {
+      ...commonTargethoursRow,
+      id: index,
+      dateUntil:
+        isLastOne && faker.datatype.boolean()
+          ? null
+          : isoDateFromDateTime(endOfMonth(new Date(to))),
+      type: TargethoursRowType.Monthly,
+      monthlyTarget: faker.datatype.number(),
+      workdayMonday: faker.datatype.boolean(),
+      workdayTuesday: faker.datatype.boolean(),
+      workdayWednesday: faker.datatype.boolean(),
+      workdayThursday: faker.datatype.boolean(),
+      workdayFriday: faker.datatype.boolean(),
+      workdaySaturday: faker.datatype.boolean(),
+      workdaySunday: faker.datatype.boolean(),
+    };
+  });
+};
+
+export const createTargethoursRowMocks = (
+  options: CommonOptions = {}
+): Array<TargethoursRow> => {
+  const {
+    count = 1,
+    dateSinceBetween: [from, to] = [DEFAULT_FROM, DEFAULT_TO],
+  } = options;
+  const fromYear = from.getFullYear();
+  // If the date range is across several years, the first year will use monthly target hours
+  const monthlyTargethoursRows =
+    fromYear === to.getFullYear()
+      ? []
+      : createTargethoursRowMonthlyMocks({
+          ...options,
+          dateSinceBetween: [from, new Date(`${fromYear}-12-31`)],
+          count: Math.min(count, 3),
+        }).filter((targethoursRow) => targethoursRow.dateUntil !== null);
+
+  const countOfWeeklyTargethoursRows = count - monthlyTargethoursRows.length;
+
+  const weeklyTargethoursRows = createTargethoursRowWeeklyMocks({
+    ...options,
+    dateSinceBetween: [new Date(`${fromYear + 1}-01-01`), new Date(to)],
+    count: countOfWeeklyTargethoursRows,
+  });
+
+  return [...monthlyTargethoursRows, ...weeklyTargethoursRows].map(
+    (targethoursRow, index) => ({
+      ...targethoursRow,
+      id: index,
+    })
+  );
+};
