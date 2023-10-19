206c81592b553dfcad377fe42927d148c72e30f9
Dominik Sumer <domi.sumer@gmail.com>
2023-10-19 15:03:32 +0200

    resolved PR comments

--- src/models/holidayscarry.mocks.ts
+++ src/models/holidayscarry.mocks.ts
@@ -1,14 +1,19 @@
 import { faker } from "@faker-js/faker";
 import { HolidayscarryRow } from "./holidayscarry.js";
 
+type Options = {
+  count?: number;
+  yearMinMax?: [number, number];
+};
+
 export const createHolidayscarryMocks = ({
   count = 1,
-  usersId = 0,
-}: { count?: number; usersId?: number } = {}) => {
+  yearMinMax = [1900, 2024],
+}: Options = {}) => {
   return Array.from({ length: count }, (): HolidayscarryRow => {
     return {
-      usersId,
-      year: faker.datatype.number({ min: 1900, max: 2024 }),
+      usersId: 0,
+      year: faker.datatype.number({ min: yearMinMax[0], max: yearMinMax[1] }),
       note: faker.datatype.boolean() ? faker.lorem.sentences(2) : null,
       count: faker.datatype.number({ min: 0, max: 100 }),
     };
