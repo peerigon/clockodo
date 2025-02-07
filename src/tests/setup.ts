e3affeac55261b2cb9c805c4d3754ed576aab233
Johannes Ewald <johannes.ewald@peerigon.com>
2025-02-07 23:56:46 +0100

    Fix tests

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -1,6 +1,9 @@
 import nock from "nock";
+import timezoneMock from "timezone-mock";
 import { afterAll, beforeEach } from "vitest";
 import { setFakerSeed } from "../lib/mocks.js";
+
+timezoneMock.register("US/Eastern");
 
 beforeEach(() => {
   setFakerSeed(123);
--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     include: ["src/**/*.test.?(c|m)[jt]s?(x)"],
-    setupFiles: ["dotenv/config", "./src/tests/vitest.ts"],
+    setupFiles: ["dotenv/config", "./src/tests/setup.ts"],
   },
   plugins: [],
 });
