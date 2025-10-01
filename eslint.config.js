d76decf7d40e155b9f9f5ecfe578df8a1dec0e93
Johannes Ewald <johannes.ewald@peerigon.com>
2025-10-01 09:43:50 +0200

    chore: Enable vitest linting rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
 import typescriptPreset from "@peerigon/configs/eslint/presets/typescript";
+import vitestRules from "@peerigon/configs/eslint/rules/vitest";
 import stylesNoDefaultExport from "@peerigon/configs/eslint/styles/no-default-export";
 
-export default [...typescriptPreset, ...stylesNoDefaultExport];
+export default [...typescriptPreset, ...vitestRules, ...stylesNoDefaultExport];
