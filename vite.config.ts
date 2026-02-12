17d12f2bc16e5ecb169b3e4806697179dd8d2ea8
Johannes Ewald <johannes.ewald@peerigon.com>
2026-02-12 14:12:27 +0100

    chore: Merge vitest configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from "vitest/config";
 
 const isCi = process.env["CI"] === "true";
+const isIntegrationTest = process.argv.at(-1) === "integration.test.ts";
 
 export default defineConfig({
   test: {
+    include: ["src/**/*.test.?(c|m)[jt]s?(x)"],
+    exclude: isIntegrationTest ? [] : ["src/integration.test.ts"],
+    setupFiles: ["dotenv/config", "./src/tests/setup.ts"],
     coverage: {
       enabled: isCi,
       reporter: ["html", "lcov"],
